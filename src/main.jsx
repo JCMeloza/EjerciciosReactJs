756c3a13f4c54e46adc7ae997c94dcd2cc986852
JuanCarlos <melozajuancarlos@gmail.com>
2025-10-17 16:54:53 +0200

    actualizar el main para rutas

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/index.css'
-import App from './App.jsx'
+// src/main.jsx
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './styles/index.css' // Aquí importas Tailwind (si lo configuraste)
+
+import App from './App'
+
+// Importa tus ejercicios
+import Ejercicio1 from './exercises/Ejercicio1'
+//import Ejercicio2 from './exercises/Ejercicio2'
+// Agrega todos los ejercicios que vayas usando
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/ejercicio1" element={<Ejercicio1 />} />
+        
+        
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 )
