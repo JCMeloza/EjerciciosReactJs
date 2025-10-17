d1439e6631302d7b3dee811b520d540e148c535d
JuanCarlos <melozajuancarlos@gmail.com>
2025-10-17 16:54:19 +0200

    modificar app para meter la lista en un componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,10 @@
-import { Link } from "react-router-dom";
+import ListaEjercicios from "./components/ListaEjercicios";
 function App() {
 	return (
 		<>
-			<div>
-				<h1>Ejercicios en React</h1>
-				<ul>
-					<li>
-						<Link to="/ejercicio1">1. Cambiador de Color</Link>
-					</li>
-					<li>
-						<Link to="/ejercicio2">2. Contador de Clics</Link>
-					</li>
-					<li>
-						<Link to="/ejercicio3">3. Lista Dinámica</Link>
-					</li>
-					<li>
-						<Link to="/ejercicio4">4. Filtro de Búsqueda</Link>
-					</li>
-					<li>
-						<Link to="/ejercicio5">5. Calculadora</Link>
-					</li>
-					<li>
-						<Link to="/ejercicio6">6. Temporizador</Link>
-					</li>
-					<li>
-						<Link to="/ejercicio7">7. Generador de Contraseñas</Link>
-					</li>
-					<li>
-						<Link to="/ejercicio8">8. Contador de Palabras</Link>
-					</li>
-					<li>
-						<Link to="/ejercicio9">9. Lista de Tareas</Link>
-					</li>
-				</ul>
-			</div>
+			<main className="min-h-screen bg-gray-100 p-4">
+				<ListaEjercicios />
+			</main>
 		</>
 	);
 }
