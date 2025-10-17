24b757cf02ff4898e0430104c5e8281d9c27f1a2
JuanCarlos <melozajuancarlos@gmail.com>
2025-10-17 17:52:34 +0200

    reiniciando el color del body

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import ListaEjercicios from "./components/ListaEjercicios";
+import { useEffect } from "react";
 function App() {
+	useEffect(() => {
+		document.body.style.backgroundColor = "white";
+	});
 	return (
 		<>
 			<main className="min-h-screen bg-gray-100 p-4">
