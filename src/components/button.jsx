811958c12c002bf078dcc6e2d4b837456357ce4b
JuanCarlos <melozajuancarlos@gmail.com>
2025-10-19 15:37:17 +0200

    cambiar color de hover sobre el botón

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Button = ({ label, onClick }) => {
 	return (
 		<button
-			className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded shadow-md transition duration-300"
+			className="bg-blue-600 hover:bg-amber-800 text-white font-semibold py-2 px-4 rounded shadow-md transition duration-300"
 			onClick={onClick}
 		>
 			{label}
